ceec85796ef05f75f3ad28c267f26ce6e3a7cbd9
Igor Khomich <igor.kh.mail@gmail.com>
2024-08-11 14:55:14 +0300

    platform addedd

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "TensorFlowLiteC",
-	platforms: [.iOS(.v15)],
+	platforms: [.iOS(.v12)],
     products: [
         .library(
             name: "TensorFlowLiteC",
